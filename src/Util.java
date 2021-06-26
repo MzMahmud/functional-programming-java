164ac256bce5d3a2b064174e27b00834eb422b50
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-06-26 12:42:18 +0600

    isPrime logic fixed

--- src/Util.java
+++ src/Util.java
@@ -21,7 +21,7 @@ public class Util {
         if (n < 2) return false;
         if (n == 2) return true;
         if (n % 2 == 0) return false;
-        for (int d = 3; d * d <= n; d++)
+        for (int d = 3; d * d <= n; d += 2)
             if (n % d == 0)
                 return false;
         return true;
