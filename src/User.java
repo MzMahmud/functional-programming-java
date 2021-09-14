10da9cc5fa67e9e575cde983346e3c9fde8c8f4d
Moaz Mahmud <moaz.mahmud.64@gmail.com>
2021-09-14 16:53:06 +0600

    Comparator example added

--- src/User.java
+++ src/User.java
@@ -1,7 +1,7 @@
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Comparator;
 
-public class User {
+public class User implements Comparable<User> {
     public static long lastId = 0;
     public long id;
     public String name;
@@ -22,10 +22,20 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-               "id=" + id +
-               ", name='" + name + '\'' +
-               ", dateOfBirth=" + dateOfBirth +
-               ", school='" + school + '\'' +
-               '}';
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", school='" + school + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(User other) {
+        // (User u) -> u.name is important because without it java has no clue about the type of the Comparator<T>
+        return Comparator.comparing((User u) -> u.name)
+                         .thenComparingInt(User::getAge)
+                         .thenComparing(u -> u.school)
+                         .thenComparingLong(u -> u.id)
+                         .compare(this, other);
     }
 }
